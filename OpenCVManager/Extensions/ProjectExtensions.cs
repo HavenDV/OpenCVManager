5e1ad647f096f230a7665dcc40e18cc20cfb3df0
HavenDV <havendv@gmail.com>
2017-11-24 16:07:49 +1000

    Added ProjectExtensions(Get/Write globals)

---
+++ OpenCVManager/Extensions/ProjectExtensions.cs
@@ -0,0 +1,22 @@
+﻿using EnvDTE;
+
+namespace OpenCVManager.Extensions
+{
+    public static class ProjectExtensions
+    {
+        public static void WrireGlobalVariable(this Project project, string name, object value)
+        {
+            project.Globals.VariablePersists[name] = true;
+            project.Globals[name] = value;
+        }
+
+        public static bool GetGlobalVariable<T>(this Project project, string name, out T value)
+        {
+            var isExists = project.Globals.VariableExists[name];
+            value = isExists ? (T)project.Globals[name] : default(T);
+
+            return isExists;
+        }
+            
+    }
+}
