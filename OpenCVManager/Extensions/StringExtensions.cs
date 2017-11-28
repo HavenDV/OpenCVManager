cfa03f37a790f04cc772f1b8bf5ee2ae2ba2f1dc
HavenDV <havendv@gmail.com>
2017-11-28 17:37:39 +1000

    Added StringExtensions.ToLines method

---
+++ OpenCVManager/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace OpenCVManager.Extensions
+{
+    public static class StringExtensions
+    {
+        public static List<string> ToLines(this string text,
+            StringSplitOptions options = StringSplitOptions.None) =>
+            text.Split(new[] { "\r\n", "\r", "\n" }, options).ToList();
+    }
+}
